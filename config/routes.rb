d9d6e9dba2266dbba5c14a4c9bf279b9b6fcd093
hiennv94 <hiennv.th@gmail.com>
2019-04-09 17:06:06 +0700

    Add config firebase

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/get_data", to: "profiles#get_data"
 end
