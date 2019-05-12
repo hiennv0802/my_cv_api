09e85cf4ea7026b471ba4aa0c7f8bb610f060fe3
hiennv0802 <hiennv.th@gmail.com>
2019-05-12 16:47:45 +0700

    Change sort data

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
   def get_data
     data = FIRE_BASE_SERVICE.get("data").body
+    data["work_schedule"] = data["work_schedule"].sort_by{|o| o["start_date"]}
+    data["basic-info"] = data["basic-info"].sort_by{|o| o["priority"]}
     render json: {
       info: data.as_json
     }
